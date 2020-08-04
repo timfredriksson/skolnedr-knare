83979f47012053599ef754e72d466f4de901932e
Tim Fredriksson <timtimmen@protonmail.ch>
2020-08-05 00:27:27 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// Set the date we're counting down to
+let countDownDate = new Date("Aug 17, 2020 08:15:00").getTime();
+
+// Update the count down every 1 second
+let x = setInterval(function() {
+
+  // Get today's date and time
+  let now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  let distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="countdownText"
+  document.getElementById("countdownText").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdownText").innerHTML = "Skolan har startat!";
+  }
+}, 1000);
